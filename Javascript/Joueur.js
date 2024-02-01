fdf97be7d46ca7e3e734014c0f2ac7fe85cc512c
Mariama1001 <154673023+Mariama1001@users.noreply.github.com>
2024-02-01 16:10:08 +0100

    Update Joueur.js

--- Javascript/Joueur.js
+++ Javascript/Joueur.js
@@ -1,1 +1,46 @@
+// joueur.js
+
+const readline = require('readline');
+
+function create_player(pid) {
+    const player = {
+        id: String(pid),
+        carpet: [], // contient des mots
+        letters: [], // contient des caractères
+        disp_carpet: function () {
+            let res = "      9  16 25 36 49 64 81\n";
+            for (let word of this.carpet) {
+                for (let letter of word) {
+                    res += letter + "  ";
+                }
+                res += "\n";
+            }
+            console.log(res);
+        },
+        disp_letters: function () {
+            let res = "Voici vos lettres à disposition : ";
+            for (let letter of this.letters) {
+                res += letter + " ";
+            }
+            console.log(res);
+        },
+        enter_word: function () {
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+
+            return new Promise(resolve => {
+                rl.question("Entrez votre mot : ", (word) => {
+                    rl.close();
+                    resolve(word.toUpperCase());
+                });
+            });
+        }
+    };
+
+    return player;
+}
+
+module.exports = { create_player };
 
